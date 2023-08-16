dea2cc44e5dccc019b04c011bb3a4ed618db94aa
Michaela-Felipe <mfelipe@diameterhealth.com>
2023-08-15 22:51:49 -0400

    Update environment vars

--- project/udagram-frontend/src/environments/environment.prod.ts
+++ project/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a68490cac47994710b942ba6dba91108-62616116.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
